51e0602fd34e3c0ae628aff101c169b7b18ecf70
Vlad2024701 <61728628+Vlad2024701@users.noreply.github.com>
2023-06-01 16:30:18 +0300

    update backend

--- reactapp/src/booking/booking.js
+++ reactapp/src/booking/booking.js
@@ -9,7 +9,17 @@ function Booking() {
     const { userStore } = React.useContext(StoreContext);
     const [user, setUser] = userStore;
     const [email, setEmail] = useState(user.email);
-    
+    const [placesCount, setPlacesCount] = useState(1);
+    const [selectedRoom, setSelectedRoom] = useState(user.email);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:5215/api/city/getCities')
+            .then(response => response.json())
+            .then(selectedRoom => {
+                setSelectedRoom(selectedRoom);
+            });
+    }, [])
+
     const { id } = useParams();
 
     const [tour, setTour] = useState(null);
@@ -17,8 +27,8 @@ function Booking() {
         fetch(`http://127.0.0.1:5215/api/tour/GetTour?id=${id}`,
             {
                 method: 'POST',
-                headers: { 
-                    "Content-Type": "application/json", 
+                headers: {
+                    "Content-Type": "application/json",
                     "Accept": "application/json"
                 }
             })
@@ -32,48 +42,48 @@ function Booking() {
     const [hotel, setHotel] = useState(null);
     useEffect(
         () => {
-        if(tour){
-        fetch(`http://127.0.0.1:5215/api/hotel/GetHotelById?id=${tour?.hotelId}`,
-            {
-                method: 'POST',
-                headers: { 
-                    "Content-Type": "application/json", 
-                    "Accept": "application/json"
-                }
-            })
-            .then(response => response.json())
-            .then(hotel => {
-                console.log(hotel);
-                setHotel(hotel);
-            });
-        }
-    }, [tour])
+            if (tour) {
+                fetch(`http://127.0.0.1:5215/api/hotel/GetHotelById?id=${tour?.hotelId}`,
+                    {
+                        method: 'POST',
+                        headers: {
+                            "Content-Type": "application/json",
+                            "Accept": "application/json"
+                        }
+                    })
+                    .then(response => response.json())
+                    .then(hotel => {
+                        console.log(hotel);
+                        setHotel(hotel);
+                    });
+            }
+        }, [tour])
 
     function componentAddBooking(event) {
         event.preventDefault()
         console.log();
         fetch(`http://localhost:5215/api/user/${user.id}/updateUser`,
-        {
+            {
                 method: 'PUT',
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify({tourId: tour.Id})
-        })
-        .then((response) => {
-            if (response.ok) {
-                return response.text();
-            }
-            throw new Error('Неверные данные');
-        })
-        .then(gotUser => {
-            console.log(gotUser);
-            alert(`Вы обновили информацию об аккаунте`);
-            userStore[1](JSON.parse(gotUser));
-        })
-        .catch((error) => {
-            alert(error.message);
-        });
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ tourId: tour.Id, placesCount: placesCount })
+            })
+            .then((response) => {
+                if (response.ok) {
+                    return response.text();
+                }
+                throw new Error('Неверные данные');
+            })
+            .then(gotUser => {
+                console.log(gotUser);
+                alert(`Вы обновили информацию об аккаунте`);
+                userStore[1](JSON.parse(gotUser));
+            })
+            .catch((error) => {
+                alert(error.message);
+            });
     }
-    
+
 
     //либо так ошибку эвэйд либо tour && tour.tourName
     if (!hotel || !tour) return (
@@ -89,25 +99,31 @@ function Booking() {
                 <form action="" method="post" name="form" className="bookingForm">
                     <label className="bookingLabel">Подтверждение почты</label>
                     <input className="bookingInput" type="text" placeholder={user.email}
-                    value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                        value={email} onChange={(e) => setEmail(e.target.value)} />
                     <label className="bookingLabel">Количество пассажиров</label>
-                    <input className="bookingInput" type="number" max={tour.places.length} min={1}  placeholder="1" />
+                    <select className="bookingInput" name="list1"
+                        onChange={(event) => { event.target.value == -1 ? setSelectedRoom(null) : setSelectedRoom(event.target.value) }}>
+                        <option value={-1}>Куда</option>
+                        {selectedRoom.map((room) => <option value={room.id}>{room.name}</option>)};
+                    </select>
+                    <input className="bookingInput" type="number" max={tour.places.length} min={1} placeholder="1"
+                        value={placesCount} onChange={(e) => setPlacesCount(e.target.value)} />
                     <label className="bookingLabel">Дата отправки</label>
-                    <input className="bookingInput" type="text" placeholder="date" disabled='true' 
-                    value={tour.tourTimeStart.substring(0,10)}/>
+                    <input className="bookingInput" type="text" placeholder="date" disabled='true'
+                        value={tour.tourTimeStart.substring(0, 10)} />
                     <label className="bookingLabel">Дата прибытия</label>
                     <input className="bookingInput" type="text" placeholder="date" disabled='true'
-                    value={tour.tourTimeEnd.substring(0,10)}/>
+                        value={tour.tourTimeEnd.substring(0, 10)} />
                     <label className="bookingLabel">Тур</label>
                     <input className="bookingInput" type="text" placeholder="Italy" disabled='true'
-                    value={tour.tourName}/>
+                        value={tour.tourName} />
                     <label className="bookingLabel">Отель</label>
                     <input className="bookingInput" type="text" placeholder="Hotels" disabled='true'
-                    value={hotel.name}/>
+                        value={hotel.name} />
                     <label className="bookingLabel">Стоимость</label>
                     <input className="bookingInput" type="text" placeholder="1000$" disabled='true'
-                    value={hotel.cost}/>
-                    <input className="bookingSubmit" type="submit" value='Бронирование'/>
+                        value={hotel.cost} />
+                    <input className="bookingSubmit" type="submit" value='Бронирование' />
                     <button className="bookingBack" type="button">
                         <Link to='/userBooked' className="bookingLink">Назад</Link>
                     </button>
